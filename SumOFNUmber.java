db778223eef8a710b05bfae44edc03a70a3a40f4
“ShyamJi-1507” <guptashyamjisg@gmail.com>
2022-09-18 14:00:25 +0530

    Sum of Two no

---
+++ SumOFNUmber.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class Sum{
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt(); 
+        int b = sc.nextInt(); 
+        int s = a+b;
+        System.out.println("Summ of no. "+  " = "+ s);
+    }
+}
